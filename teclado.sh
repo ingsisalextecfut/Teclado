1f0e807a411892f7d7c74ec8da5ca532b45d0373
Alex <ingsisalexbtc@gmail.com>
2020-12-03 22:40:34 -0600

    Teclado Versión 1.0

---
+++ teclado.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.termux && echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP','DEL'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','BKSP']]" > ~/.termux/termux.properties
+termux-reload-settings
